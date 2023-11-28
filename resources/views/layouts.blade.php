267fd9050ea2b5662d2c7f4b88e9b7f328a75f16
Ghel12 <angelmaecarpeso@gmail.com>
2023-11-28 14:06:32 +0800

    nov_28_2023-1stcommit

---
+++ resources/views/layouts.blade.php
@@ -0,0 +1,17 @@
+
+<h3>
+    Showing all Appointment Records
+</h3>
+<hr>
+<table id="appointmentsall" class="display" cellspacing="0">
+    <thead>
+        <tr>
+            <th>Show Patient Record</th>
+            <th>Edit Appointment</th>
+            <th>Show Appointment</th>
+            <th>Delete Appointment</th>
+            <th>Appointment Information</th>
+        </tr>
+    </thead>
+    <tbody>
+      
